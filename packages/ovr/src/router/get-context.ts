0aee4e33d836e9adc7290f9a640c5ab6ad41a432
rossrobino <rbrobino@gmail.com>
2025-05-09 13:41:06 -0400

    undo

--- packages/ovr/src/router/get-context.ts
+++ packages/ovr/src/router/get-context.ts
@@ -2,7 +2,9 @@ import type { Context } from "./context.js";
 import type { Params } from "./index.js";
 import { AsyncLocalStorage } from "node:async_hooks";
 
-export const asyncLocalContext = new AsyncLocalStorage<Context<any, Params>>();
+export const asyncLocalContext = new AsyncLocalStorage<
+	Context<unknown, Params>
+>();
 
 /** Helper to get the current context within a handler. */
 export const context = () => {
