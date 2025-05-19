4cd2268e3cf702f9eea88347e426b14099614944
rossrobino <rbrobino@gmail.com>
2025-05-19 18:23:24 -0400

    test

--- apps/docs/src/server/+app.tsx
+++ apps/docs/src/server/+app.tsx
@@ -3,18 +3,13 @@ import * as test from "./test";
 import { html } from "client:page";
 import { App } from "ovr";
 
-console.log("top level");
-
 const app = new App();
 
 app.use(async (c, next) => {
-	console.log("use");
 	c.base = html;
 	await next();
 });
 
 app.add(home, test);
-
-app.prerender = [];
 
 export default app;
