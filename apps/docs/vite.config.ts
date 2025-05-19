7c14e82f384cc92900b85dfe1581b44c403fbff0
rossrobino <rbrobino@gmail.com>
2025-05-19 18:11:14 -0400

    vercel

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -1,3 +1,4 @@
+import { adapter } from "@domcojs/vercel";
 import { md } from "@robino/md";
 import langBash from "@shikijs/langs/bash";
 import langTsx from "@shikijs/langs/tsx";
@@ -6,7 +7,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [
-		domco(),
+		domco({ adapter: adapter() }),
 		md({
 			highlighter: {
 				langs: [langTsx, langBash],
